c7c75924a97e635576a146ae5a5204dbe57dba7d
Aman Fatehpuria <afatehpuria@hmc.edu>
2018-06-21 19:09:21 -0700

    first commit

---
+++ src/sorting.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aman Fatehpuria on 6/20/18.
+//
+
+#include "sorting.h"
---
+++ src/sorting.h
@@ -0,0 +1,29 @@
+//
+// Created by Aman Fatehpuria on 6/20/18.
+//
+
+#ifndef ALGORITHMS_SORTING_H
+#define ALGORITHMS_SORTING_H
+
+#include <iostream>
+#include <stdio.h>
+
+class sorting {
+public:
+    /**
+     *\brief class containing various sorting algorithms
+     * \note constructor for sorting array of ints
+     * @param arr array of integers to be sorted
+     */
+    sorting(int arr[]);
+
+    /**
+     * \note constructor for sorting floats
+     * @param arr array of floats to be sorted
+     */
+    sorting(float arr[]);
+
+    void BubbleSort
+};
+
+#endif //ALGORITHMS_SORTING_H
